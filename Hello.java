b46183fb2011b1a4a6582a43a058bf2f45e6947b
Chrimbs <heinc2021@york.org>
2020-09-23 10:03:50 -0700

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello{
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         //Use a comment like this to write your name!
         //Like this: Ryo
+        //Chris
     }
 }
